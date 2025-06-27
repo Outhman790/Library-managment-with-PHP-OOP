4f2d4cefbb7cc56d52269becf918519a4a853496
Outhman Moumou <tangerino2011@gmail.com>
2025-06-27 12:11:40 +0100

    refactor: improve myBorrowings page layout and structure
    - Introduced flex column layout to keep footer at bottom of page
    - Wrapped main content in <main> for semantic correctness
    - Replaced redundant session nickname logic with null coalescing
    - Cleaned up HTML structure and indentation
    - Applied consistent styling and spacing

--- my_borrowings.php
+++ my_borrowings.php
@@ -6,38 +6,43 @@ checkAccount($_SESSION['Penalty_Count']);
 if ($_SESSION['Admin'] === 0) :
 ?>
 
-    <!DOCTYPE html>
-    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>My borrowings</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Bootstrap icons-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>My borrowings</title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.1.96/css/materialdesignicons.min.css" rel="stylesheet" />
+    <style>
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+    }
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-        <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.1.96/css/materialdesignicons.min.css" rel="stylesheet" />
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-        </script>
+    .page-wrapper {
+        min-height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
 
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
-        </script>
-    </head>
+    main {
+        flex: 1;
+    }
+    </style>
+</head>
 
-    <body>
-
-        <!-- Navigation-->
+<body>
+    <div class="page-wrapper">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
                 <a class="navbar-brand" href="#">Olibrary</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -47,58 +52,45 @@ if ($_SESSION['Admin'] === 0) :
                         </li>
                     </ul>
                     <div class="dropdown">
-                        <?php
-                        if ($_SESSION['Nickname']) :
-                        ?>
-                            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                <?php echo $_SESSION['Nickname'] ?>
-                            </a>
-                        <?php
-                        else :
-                        ?>
-                            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                                Profile nickname
-                            </a>
-                        <?php
-                        endif;
-                        ?>
-                        <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            <?php echo $_SESSION['Nickname'] ?? 'Profile nickname'; ?>
+                        </a>
+                        <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="myProfile.php">My profile</a></li>
                             <li><a class="dropdown-item" href="my_reservations.php">My reservations</a></li>
                             <li><a class="dropdown-item" href="#">My borrowings</a></li>
-                            <li><a href="./includes/logout.inc.php" class="dropdown-item" href="#">Logout</a></li>
+                            <li><a href="./includes/logout.inc.php" class="dropdown-item">Logout</a></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </nav>
-        <!-- Header-->
+
         <header class="bg-dark py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
                     <h1 class="display-4 fw-bolder">Shop in style</h1>
-                    <p class="lead fw-normal text-white-50 mb-0">
-                        With this shop homepage template
-                    </p>
+                    <p class="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
                 </div>
             </div>
         </header>
-        <!-- Section-->
-        <section class="py-5">
-            <div class="container px-4 px-lg-5 mt-5">
-                <!--Section: Content-->
-                <section class="text-center">
-                    <h4 class="mb-5"><strong>My Borrowings</strong></h4>
-                    <div class="row">
-                        <?php
-                        $myReservationsObj = new getMyBorrowings($_SESSION['Nickname']);
-                        $myReservations = $myReservationsObj->getMyBorrowings();
-                        foreach ($myReservations as $key => $value) :
-                        ?>
+
+        <main>
+            <section class="py-5">
+                <div class="container px-4 px-lg-5 mt-5">
+                    <section class="text-center">
+                        <h4 class="mb-5"><strong>My Borrowings</strong></h4>
+                        <div class="row">
+                            <?php
+                                $myReservationsObj = new getMyBorrowings($_SESSION['Nickname']);
+                                $myReservations = $myReservationsObj->getMyBorrowings();
+                                foreach ($myReservations as $key => $value) :
+                                ?>
                             <div class="col-lg-3 col-md-4 mb-4">
                                 <div class="card">
-                                    <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
-                                        <img src="img/<?php echo $value['Cover_Image'] ?>" class="img-fluid-custom mw-100" style="height: 23rem;" />
+                                    <div class="bg-image hover-overlay ripple">
+                                        <img src="img/<?php echo $value['Cover_Image'] ?>"
+                                            class="img-fluid-custom mw-100" style="height: 23rem;" />
                                     </div>
                                     <div>
                                         <h5 class="card-title mt-2"><?php echo $value['Title'] ?></h5>
@@ -119,24 +111,23 @@ if ($_SESSION['Admin'] === 0) :
                                     </div>
                                 </div>
                             </div>
-                        <?php endforeach; ?>
-                    </div>
-                </section>
-            </div>
-        </section>
-        <!-- Footer-->
-        <footer class="py-5 bg-dark">
-            <div class="container">
-                <p class="m-0 text-center text-white">
-                    Copyright &copy; Your Website 2023
-                </p>
-            </div>
-        </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <script src="js/scripts.js"></script>
-    </body>
+                            <?php endforeach; ?>
+                        </div>
+                    </section>
+                </div>
+            </section>
+        </main>
 
-    </html>
+        <footer class="bg-dark text-white text-center py-4">
+            &copy; <?php echo date('Y'); ?> Your Website
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/scripts.js"></script>
+</body>
+
+</html>
 <?php
 else :
     header('location: admin/all_borrowings.php');
