5ffad5b3f6097d2d375a8e56b0008ef2bbb70325
Outhman Moumou <tangerino2011@gmail.com>
2025-07-04 21:54:52 +0100

    chore(cron): add logging and working directory setup to cron script
    - Added `echo` statements for start and end timestamps
    - Set working directory before running PHP script
    - Redirected output and errors to cron_log.txt

--- AutoScript/runscript.bat
+++ AutoScript/runscript.bat
@@ -1,1 +1,6 @@
-"C:\xampp\php\php.exe" -f "C:\xampp\htdocs\Library website\Crone.php"
+@echo off
+echo Starting Library Management Cron Job - %date% %time%
+cd /d "C:\xampp\htdocs\Library-managment-with-PHP-OOP"
+"C:\xampp\php\php.exe" -f "Crone.php" >> "cron_log.txt" 2>&1
+echo Cron job completed - %date% %time%
+pause
