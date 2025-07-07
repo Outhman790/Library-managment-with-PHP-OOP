9c7aba1472def319529e4cc9ae2771c93a592385
Outhman Moumou <tangerino2011@gmail.com>
2025-07-07 02:39:13 +0100

    chore(hero): update hero background image filename in homepage to hero-bg-homepage.jpg

--- index.php
+++ index.php
@@ -162,7 +162,7 @@ $randomItems = $libraryObj->getRandomItems(4);
         <!--  -->
         <!-- Background image -->
         <div id="intro" class="bg-image shadow-2-strong d-flex align-items-center justify-content-center text-center"
-            style="background-image: url('img/hero-bg-home.jpg'); height: 70vh; background-size: cover; background-position: center;">
+            style="background-image: url('img/hero-bg-homepage.jpg'); height: 70vh; background-size: cover; background-position: center;">
             <div class="mask" style="background-color: rgba(0, 0, 0, 0.8)">
                 <div class="container d-flex align-items-center justify-content-center text-center h-100">
                     <div class="text-white">
